37b52d0298751f351135dbdc4d84b26caaa6fe9f
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2021-01-13 16:26:44 -0300

    add FindCategoryById and exception handling

--- backend/src/main/java/com/devgabriel/dgcatalog/services/CategoryService.java
+++ backend/src/main/java/com/devgabriel/dgcatalog/services/CategoryService.java
@@ -3,11 +3,13 @@ package com.devgabriel.dgcatalog.services;
 import com.devgabriel.dgcatalog.dtos.CategoryDTO;
 import com.devgabriel.dgcatalog.entities.Category;
 import com.devgabriel.dgcatalog.repositories.CategoryRepository;
+import com.devgabriel.dgcatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,6 +22,13 @@ public class CategoryService {
 	public List<CategoryDTO> findAll() {
 		List<Category> categories = repository.findAll();
 		return categories.stream().map(CategoryDTO::new).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category category = obj.orElseThrow(() -> new EntityNotFoundException("Category not found"));
+		return new CategoryDTO(category);
 	}
 
 }
