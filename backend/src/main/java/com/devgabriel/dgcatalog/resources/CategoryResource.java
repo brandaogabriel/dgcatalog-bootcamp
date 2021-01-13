7b3ffaca884da3de84db8afde4095e6066253ed4
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2021-01-13 15:07:35 -0300

    add CategoryDTO

--- backend/src/main/java/com/devgabriel/dgcatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devgabriel/dgcatalog/resources/CategoryResource.java
@@ -1,6 +1,6 @@
 package com.devgabriel.dgcatalog.resources;
 
-import com.devgabriel.dgcatalog.entities.Category;
+import com.devgabriel.dgcatalog.dtos.CategoryDTO;
 import com.devgabriel.dgcatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +18,8 @@ public class CategoryResource {
 	private CategoryService service;
 
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
