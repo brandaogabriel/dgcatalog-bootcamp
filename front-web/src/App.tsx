9786382a7f569ea503f662cb8536e7087d46d8d6
brandaogabriel <gabriel.brandao@ccc.ufcg.edu.br>
2021-01-20 16:01:08 -0300

    feat: add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import './core/assets/styles/custom.scss';
+import './app.scss';
+import Navbar from './core/components/Navbar';
+
 const App = (): JSX.Element => {
-  return <h1>App</h1>;
+  return <Navbar />;
 };
 
 export default App;
