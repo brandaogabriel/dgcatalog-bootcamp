bb34a3763e023f86332f49553fd70e33354f6c3c
Gabriel Brandão <gabriel.brandao@ccc.ufcg.edu.br>
2021-03-08 21:48:44 -0300

    feat: add Auth component

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -7,6 +7,7 @@ import Admin from './pages/Admin';
 import Catalog from './pages/Catalog';
 import ProductDetails from './pages/Catalog/components/ProductDetails';
 import Home from './pages/Home';
+import Auth from 'pages/Auth';
 
 const Routes = (): JSX.Element => (
   <BrowserRouter>
@@ -20,6 +21,10 @@ const Routes = (): JSX.Element => (
       </Route>
       <Route path="/products/:id">
         <ProductDetails />
+      </Route>
+      <Redirect from="/admin/auth" to="/admin/auth/login" exact />
+      <Route path="/admin/auth">
+        <Auth />
       </Route>
       <Redirect from="/admin" to="/admin/products" exact />
       <Route path="/admin">
