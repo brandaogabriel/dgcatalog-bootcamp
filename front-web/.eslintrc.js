50c58e692865d3f1e4a38a247675b5e47cda29df
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2021-03-14 13:24:14 -0300

    fix: add rule into .esrclint.js

--- front-web/.eslintrc.js
+++ front-web/.eslintrc.js
@@ -24,5 +24,11 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
+    'prettier/prettier': [
+      "error",
+      {
+        "endOfLine": "auto"
+      }
+    ],
   },
 };
