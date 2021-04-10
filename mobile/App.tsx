86dabf7e7c194d33e93bb51c45e22145c2c15c61
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2021-04-09 21:59:12 -0300

    chore: mobile project created

---
+++ mobile/App.tsx
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = (): JSX.Element => {
+  return (
+    <View>
+      <Text>Olá!</Text>
+    </View>
+  );
+}
+
+export default App;
